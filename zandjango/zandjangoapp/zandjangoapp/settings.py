4c002f6d8505507ecf3c7cfb47f33ef21253b780
Zanexample <109088937+Zanexample@users.noreply.github.com>
2023-09-02 11:14:11 +0000

    django-update2

--- zandjango/zandjangoapp/zandjangoapp/settings.py
+++ zandjango/zandjangoapp/zandjangoapp/settings.py
@@ -118,7 +118,20 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+  
+]
+
+JAZZMIN_SETTINGS = {
+
+ "site_header": "admin",
+ "site_logo": "/img/icon/Xlogo-icon.png",
+
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
